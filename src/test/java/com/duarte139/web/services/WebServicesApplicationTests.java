65ebb4e9d995bcd04f8950f3920b0f83c4168039
duarte139 <duarte139@gmail.com>
2025-01-27 20:15:50 -0300

    Inicio do Projeto

---
+++ src/test/java/com/duarte139/web/services/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duarte139.web.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
