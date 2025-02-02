446f141cb6733ee88d68f3671b450952979a9b6d
duarte139 <duarte139@gmail.com>
2025-02-02 14:15:40 -0300

    Service layer, component registration

--- src/main/java/com/duarte139/web/services/resources/UserResource.java
+++ src/main/java/com/duarte139/web/services/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.duarte139.web.services.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.duarte139.web.services.UserService;
 import com.duarte139.web.services.entity.User;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User( 1L,"Maria","maria.gmail.com","9999999","12345");
-		return  ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return  ResponseEntity.ok().body(list);
+	}
+	
+	public ResponseEntity<User> findByid(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 
 }
